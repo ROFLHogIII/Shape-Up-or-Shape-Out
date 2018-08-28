d657349625030afd5cd06d86210626b718ebd828
Richard Garner <richardgarner@mac.com>
2018-08-27 19:29:46 -0500

    squares appear inside box

--- app.js
+++ app.js
@@ -1,61 +1,89 @@
+let MAX = 600;
 class Shape {
     constructor(height, length) {
         this.height = height;
         this.length = length;
     };
     getArea() {
-        area = (this.length * this.height)
+        return (this.length * this.height)
     }
     getPerimeter() {
-        per = 2(this.length + this.height)
+        return 2(this.length + this.height)
     }
-}
+};
 
 class Square extends Shape {
-    constructor (height, length){
-        super (height, length)
+    constructor(height, length) {
+        super(height, length)
     }
-}
-let square = new Square(height, length)
-square.getArea()
-square.getPerimeter()
+};
+//let square = new Square(height, length)
+//square.getArea()
+//square.getPerimeter()
 
-class Rectangle extends Shape{
-    constructor (height, length){
+class Rectangle extends Shape {
+    constructor(height, length) {
         super(height, length)
     }
 }
-let rectangle = new Rectangle(height, length)
-rectangle.getArea()
-rectangle.getPerimeter()
+//let rectangle = new Rectangle(height, length)
+//rectangle.getArea()
+//rectangle.getPerimeter()
 
-class Circle extends Shape{
-    constructor(height){
-        super (height)
+class Circle extends Shape {
+    constructor(height) {
+        super(height)
     }
     getArea() {
-        area = ( 3.14 * this.height * this.height)
+        return (3.14 * this.height * this.height)
     }
     getPerimeter() {
-        per = 2 * 3.14 * (this.height)
+        return 2 * 3.14 * (this.height)
     }
 }
-let circle = new Circle(height)
-circle.getArea()
-circle.getPerimeter()
+//let circle = new Circle(height)
+//circle.getArea()
+//circle.getPerimeter()
 
-class Triangle extends Shape{
-    constructor(height){
-        super (height)
+class Triangle extends Shape {
+    constructor(height) {
+        super(height)
     }
     getArea() {
-        area = (this.height * this.height)/2
+        return (this.height * this.height) / 2
     }
     getPerimeter() {
         let hyp = Math.sqrt(Math.pow(this.height, 2) * 2);
-        per = (2 * (this.height)) + hyp
+        return (2 * (this.height)) + hyp
     }
 }
-let triangle = new Triangle(height)
-triangle.getArea()
-triangle.getPerimeter()
+//let triangle = new Triangle(height)
+//triangle.getArea()
+//triangle.getPerimeter()
+
+$(`#squ`).click(function () {
+    event.preventDefault()
+    let height = $(`#side-s`).val()
+    let length = $(`#side-s`).val()
+    let sq = new Square(height, length)
+    let xVal = ranVal(0, MAX)
+    let yVal = ranVal(0, MAX)
+    $(`<div class="square" style="left:${xVal}px; top:${yVal}px; height: ${height}em; width:${length}em;"></div>`).appendTo(`#shape-box`)
+    console.log($(`#side-s`).val())
+    
+})
+
+function ranVal(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function appendBox(){
+    (`.shape-info`).empty()
+    area = this.getArea();
+    perm = this.getPerimeter();
+    h = this.height
+    l = this.length
+    $(`<div>Area: ${area}</div>`).appendTo(`#Area`)
+    $(`<div>Perimiter: ${perm}</div>`).appendTo(`#Perimeter`)
+    $(`<div>Width: ${l}</div>`).appendTo(`#Width`)
+    $(`<div>Height: ${h}</div>`).appendTo(`#Height`)
+};

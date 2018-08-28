d81f28f2ffd9d6a0948d4e87a57463b6174a5fc3
Richard Garner <richardgarner@mac.com>
2018-08-27 22:11:28 -0500

    circles now fuction properly

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ class Square extends Shape {
         this.div = $(`<div class="square" style="left:${xVal}px; top:${yVal}px; height: ${this.height}em; width:${this.length}em;"></div>`).appendTo(`#shape-box`);
         this.div.click(function () {
             $(`.shape-info`).empty()
+            $(`<div>Type: Square</div>`).appendTo(`#Shape-Name`)
             $(`<div>Area: ${area}</div>`).appendTo(`#Area`)
             $(`<div>Perimeter: ${perm}</div>`).appendTo(`#Perimeter`)
             $(`<div>Width: ${this.length}</div>`).appendTo(`#Width`)
@@ -41,6 +42,7 @@ class Rectangle extends Shape {
         this.div = $(`<div class="rectangle" style="left:${xVal}px; top:${yVal}px; height: ${this.height}em; width:${this.length}em;"></div>`).appendTo(`#shape-box`);
         this.div.click(function () {
             $(`.shape-info`).empty()
+            $(`<div>Type: Rectangle</div>`).appendTo(`#Shape-Name`)
             $(`<div>Area: ${area}</div>`).appendTo(`#Area`)
             $(`<div>Perimeter: ${perm}</div>`).appendTo(`#Perimeter`)
             $(`<div>Width: ${this.length}</div>`).appendTo(`#Width`)
@@ -52,12 +54,25 @@ class Rectangle extends Shape {
 class Circle extends Shape {
     constructor(height) {
         super(height)
+        let Radius = height;
+        let xVal = ranVal(0, MAX);
+        let yVal = ranVal(0, MAX);
+        let area = this.getArea();
+        let perm = this.getPerimeter();
+        this.div = $(`<div class="circle" style="left:${xVal}px; top:${yVal}px; height: ${this.height * 2}em; width:${this.height * 2}em;"></div>`).appendTo(`#shape-box`);
+        this.div.click(function () {
+            $(`.shape-info`).empty()
+            $(`<div>Type: Circle</div>`).appendTo(`#Shape-Name`)
+            $(`<div>Area: ${area}</div>`).appendTo(`#Area`)
+            $(`<div>Perimeter: ${perm}</div>`).appendTo(`#Perimeter`)
+            $(`<div>Radius: ${Radius}</div>`).appendTo(`#Radius`)
+        }.bind(this))
     }
     getArea() {
-        return (3.14 * this.height * this.height)
+        return 3.14 * Number(this.height) * Number(this.height)
     }
     getPerimeter() {
-        return 2 * 3.14 * (this.height)
+        return 2 * 3.14 * Number(this.height)
     }
 }
 //let circle = new Circle(height)
@@ -85,14 +100,18 @@ $(`#squ`).click(function () {
     let height = $(`#side-s`).val()
     let length = $(`#side-s`).val()
     let sq = new Square(height, length)
-    console.log($(`#side-s`).val())
 })
 $(`#rec`).click(function () {
     event.preventDefault()
     let height = $(`#height-r`).val()
     let length = $(`#length-r`).val()
     let rec = new Rectangle(height, length)
-    console.log($(`#side-s`).val())
+})
+$(`#cir`).click(function () {
+    event.preventDefault()
+    let height = ($(`#radius`).val())
+    let cir = new Circle(height)
+    console.log(height)
 })
 
 function ranVal(min, max) {
